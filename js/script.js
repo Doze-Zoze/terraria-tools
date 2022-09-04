5c17dd392ac42368f5046903de4e65a7e91ee122
Doze-Zoze <43045606+Doze-Zoze@users.noreply.github.com>
2022-09-04 13:59:37 -0500

    calc damage plus theming

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 import { weapons } from './weapon.js';
 
  document.getElementById("NameSearch").addEventListener("click", search);
+ document.getElementById("DamageCalculate").addEventListener("click", damageCalc);
+
 
 function search() {
     var weaponName = document.getElementById("weaponName").value;
@@ -12,6 +14,16 @@ function search() {
 
 }
 
+function damageCalc() {
+    var damage = document.getElementById("weaponDamage").value;
+    var crit = document.getElementById("weaponCrit").value;
+    var usetime = document.getElementById("weaponUseTime").value;
+    var defense = document.getElementById("enemyDefense").value;
+    var dph = (damage-defense)+crit(damage-defense);
+    var dps = dph*(60/usetime);
+    document.getElementById("damageOut").value = dph;
+    document.getElementById("DPSOut").value = dps;
+}
 var select = document.getElementById('weaponName');
 for (var i = 0; i < weapons.length; i++){
     console.log(i)
